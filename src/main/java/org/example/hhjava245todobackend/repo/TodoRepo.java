2fa6c9c3e8f97e4588245b6658c21e19822c194b
ridvanberisa <ridvan.berisa@protonmail.com>
2025-04-30 11:14:10 +0200

    Commint adjustment Repo

--- src/main/java/org/example/hhjava245todobackend/repo/TodoRepo.java
+++ src/main/java/org/example/hhjava245todobackend/repo/TodoRepo.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepo extends MongoRepository<Todo, String> {
+
 }
--- src/test/java/org/example/hhjava245todobackend/HhJava245TodoBackendApplicationTests.java
+++ src/test/java/org/example/hhjava245todobackend/HhJava245TodoBackendApplicationTests.java
@@ -1,6 +1,7 @@
 package org.example.hhjava245todobackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
